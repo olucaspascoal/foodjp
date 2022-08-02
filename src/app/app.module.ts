d0c5a3541ab2b4ee111d77a1849aa9f04bb0793d
Lucas Pascoal <olucaspascoal@gmail.com>
2022-08-01 23:18:40 -0300

    Criação dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { PopularComponent } from './popular/popular.component';
+import { ComentariosComponent } from './comentarios/comentarios.component';
+import { LocalizacaoComponent } from './localizacao/localizacao.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    SobreComponent,
+    PopularComponent,
+    ComentariosComponent,
+    LocalizacaoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
