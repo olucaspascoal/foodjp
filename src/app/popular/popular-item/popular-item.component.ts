9af0581d2618d0fc36bdcc7acf4e69ac52ddfa00
Lucas Pascoal <olucaspascoal@gmail.com>
2022-08-07 23:12:51 -0300

    Desenvolvimento de itens populares

--- src/app/popular/popular-item/popular-item.component.ts
+++ src/app/popular/popular-item/popular-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-popular-item',
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PopularItemComponent implements OnInit {
 
   constructor() { }
+  @Input() data: any;
 
   ngOnInit(): void {
   }
