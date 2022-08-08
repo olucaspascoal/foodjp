9f87678548b1a1f97c29dda872c56be7be125d0a
Lucas Pascoal <olucaspascoal@gmail.com>
2022-08-07 23:39:54 -0300

    informações mockadas no item de produtos

--- src/app/popular/popular.component.ts
+++ src/app/popular/popular.component.ts
@@ -8,8 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class PopularComponent implements OnInit {
 
   constructor() { }
+  public produtos = [{}];
 
   ngOnInit(): void {
+    this.produtos = [
+      {
+        imagem: '/assets/pratos/1.png',
+        titulo: 'Ramen de Frango',
+        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        preco: 'R$ 30'
+      },
+      {
+        imagem: '/assets/pratos/2.png',
+        titulo: 'Ramen Apimentado',
+        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        preco: 'R$ 30'
+      },
+      {
+        imagem: '/assets/pratos/3.png',
+        titulo: 'Ramen Tradicional',
+        texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        preco: 'R$ 30'
+      }
+    ]
   }
 
 }
